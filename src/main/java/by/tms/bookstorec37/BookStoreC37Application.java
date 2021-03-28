dc9506645b5d171fd4a30c7f7254aa0482d9643e
Simon Pirko <sipi7gm@gmail.com>
2021-03-28 14:39:40 +0300

    first commit

---
+++ src/main/java/by/tms/bookstorec37/BookStoreC37Application.java
@@ -0,0 +1,13 @@
+package by.tms.bookstorec37;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreC37Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreC37Application.class, args);
+	}
+
+}
---
+++ src/test/java/by/tms/bookstorec37/BookStoreC37ApplicationTests.java
@@ -0,0 +1,13 @@
+package by.tms.bookstorec37;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreC37ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
