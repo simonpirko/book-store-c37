92178fe2065bddf0993c8b6c9113aa51a6fa2bdf
Ivan Zubkov <you@example.com>
2021-03-31 19:07:25 +0300

    Added: Methods in OrderRepository Methods in UserRepository

--- src/main/java/by/tms/bookstorec37/repository/OrderRepository.java
+++ src/main/java/by/tms/bookstorec37/repository/OrderRepository.java
@@ -1,7 +1,22 @@
 package by.tms.bookstorec37.repository;
 
-import by.tms.bookstorec37.entity.Category;
+import by.tms.bookstorec37.entity.Book;
+import by.tms.bookstorec37.entity.Order;
+import by.tms.bookstorec37.entity.OrderStatus;
+import by.tms.bookstorec37.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface OrderRepository extends JpaRepository<Category, Long> {
+import java.util.List;
+import java.util.Set;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    List<Order> findOrdersByUser(User user);
+    List<Order> findOrdersByOrderStatus(OrderStatus orderstatus);
+    List<Order> findOrdersByBookListIsContaining(Book book);
+    void deleteOrdersByUser(User user);
+    void deleteOrdersByOrderStatus (OrderStatus orderstatus);
 }
+
--- src/main/java/by/tms/bookstorec37/repository/UserRepository.java
+++ src/main/java/by/tms/bookstorec37/repository/UserRepository.java
@@ -1,7 +1,14 @@
 package by.tms.bookstorec37.repository;
 
 import by.tms.bookstorec37.entity.User;
+import com.zaxxer.hikari.util.ClockSource;
+import net.bytebuddy.asm.Advice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    User getUserByUsername(String username);
+    User getUserById(long id);
+    boolean existsUserByUsername(String username);
+    void deleteUserByUsername(String username);
 }
